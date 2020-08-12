5525eb5254cce095b864587ff3b25f49029e2825
HeatherJane <heatherjane.moore@gmail.com>
2020-08-12 13:08:46 -0400

    Updating

--- static 2/js/logic2.js
+++ static 2/js/logic2.js
@@ -3,7 +3,7 @@ console.log("Step 2 working");
 // We create the tile layers that will be the selectable backgrounds of our map.
 // One for our grayscale background.
 
-var apiKey = "YOUR API KEY HERE!";
+var apiKey = "sk.eyJ1IjoiaGVhdGhlcmphbmVtb29yZSIsImEiOiJja2N2OGYzbTAwMXMyMnVwZXFqanRndXRoIn0.lRTIPaCACYvLOx-ogAefaA";
 
 var graymap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,7 +1,7 @@
 // We create the tile layer that will be the background of our map.
 console.log("Step 1 working");
 
-var apiKey = "YOUR API KEY HERE!";
+var apiKey = "sk.eyJ1IjoiaGVhdGhlcmphbmVtb29yZSIsImEiOiJja2N2OGYzbTAwMXMyMnVwZXFqanRndXRoIn0.lRTIPaCACYvLOx-ogAefaA";
 
 var graymap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
